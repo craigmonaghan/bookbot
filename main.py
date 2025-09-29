730dcda91f985ffd865146279599d7d5a1d53232
craigmonaghan <craigmonaghan.op@gmail.com>
2025-09-30 00:50:16 +0100

    completed

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from stats import get_num_words, get_char_count, sorting
+import sys
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
     
-    get_num_words(filepath)
-    get_char_count(filepath)
+    filepath = sys.argv[1]
     
     word_count = get_num_words(filepath)
     print(f"Found {word_count} total words")
@@ -15,5 +17,7 @@ def main():
     for item in sorted_chars:
         if item["char"].isalpha():
             print(f"{item['char']}: {item['num']}")
-    
-main()
+
+#check if its being run directly and not imported then ran?
+if __name__ == "__main__":
+    main()
